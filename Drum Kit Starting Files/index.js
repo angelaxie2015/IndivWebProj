f3156daa255e46e08230332149f4259064e2f18b
angelaxie2015 <angelaxie2015@gmail.com>
2021-02-03 07:05:40 -0500

    add sound to website

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,47 @@
+function playDrum (id){
+	switch(id){
+		case "w":
+			var sound = new Audio("sounds/tom-1.mp3");
+		break;
+
+		case "a":
+			var sound = new Audio("sounds/tom-2.mp3");
+		break;
+
+		case "s":
+			var sound = new Audio("sounds/tom-3.mp3");
+		break;
+
+		case "d":
+			var sound = new Audio("sounds/tom-4.mp3");
+		break;
+
+		case "j":
+			var sound = new Audio("sounds/crash.mp3");
+		break;
+
+		case "k":
+			var sound = new Audio("sounds/snare.mp3");
+		break;
+
+		case "l":
+			var sound = new Audio("sounds/kick-bass.mp3");
+		break;
+
+		default:
+			console.log(this.innerHTML);
+	}
+	sound.play();
+}
+var buttons = document.querySelectorAll(".drum");
+for(var i = 0; i < buttons.length; i++){
+	buttons[i].addEventListener("click", function () {
+		playDrum(this.innerHTML);
+		
+	});
+}
+
+document.addEventListener("keydown", function (event) {
+	playDrum(event.key);
+	
+});
